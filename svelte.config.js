b0a8020037b3ba2059d5b14432be7823270c375a
Ян Козыренко <kopn48@mail.ru>
2022-07-06 23:53:31 +0300

    edit config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
 import adapter from '@sveltejs/adapter-auto';
-
 import preprocess from 'svelte-preprocess';
 
 const dev = process.env.NODE_ENV === 'development';
@@ -7,13 +6,16 @@ const dev = process.env.NODE_ENV === 'development';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
-	kit: {
-		adapter: adapter()
-	},
-	paths: {
-		base: dev ? '' : '/',
-	},
-	appDir: 'internal',
-};
 
+	kit: {
+		adapter: adapter(),
+		prerender: {
+			default: false
+		},
+		paths: {
+			base: dev ? '' : '/your-repo-name',
+		},
+		appDir: 'internal',
+	}
+};
 export default config;
