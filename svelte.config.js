e3e2c52d287a5bc28304bcfe69cc690ee603e34d
Ян Козыренко <kopn48@mail.ru>
2022-07-06 23:25:15 +0300

    build

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static"; 
+
 import preprocess from 'svelte-preprocess';
 
 const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
+	adapter: adapter({
+		pages: "docs",
+		assets: "docs"
+	}),
 	preprocess: preprocess(),
 	kit: {
 		adapter: adapter()
 	},
 	paths: {
-		base: dev ? '' : '/',
+		base: dev ? '' : '/orillio.github.io',
 	},
+	target: "#svelte"
 };
 
 export default config;
