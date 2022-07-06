22c555f1b0841258fb5348240a5c3b071c77f769
Ян Козыренко <kopn48@mail.ru>
2022-07-07 01:16:05 +0300

    fix

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <script lang="ts" context="module">
 	import Body from "../components/mainbody.svelte"
-	export const prerender = false;
+	export const prerender = true;
 </script>
 
 <Body headerName="Yan Kozyrenko"/>
